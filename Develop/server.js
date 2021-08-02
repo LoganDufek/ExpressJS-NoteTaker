dc160178a5c6ef32651ec0592edbec76e13c2059
Logan D <logandufek@gmail.com>
2021-08-02 10:11:27 -0400

    POST route sucessfully added and functional. Ability to delete notes based on ID (assigned in POST route) added sucessfully as well. Comments to come

--- Develop/server.js
+++ Develop/server.js
@@ -22,6 +22,54 @@ app.get('/api/notes', (req, res) => {
   
 })
 
+app.post('/api/notes', (req, res) => { 
+  req.body.id = note.length.toString();
+
+  const newNote = req.body
+
+
+ console.log(newNote)
+ note.push(newNote)
+
+
+  res.json(note);
+
+  fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify(note, null, 1)
+    );
+  })
+
+  const findNoteByID = (id) => { 
+
+    const matchingID = note.find((notes) => {
+      return notes.id === id
+    })
+
+    return matchingID
+  }
+
+
+  app.delete('/api/notes/:id', (req, res) => {
+    
+   
+    const id = req.params.id
+    const removedNote = findNoteByID(id)
+
+    const index = note.indexOf(removedNote);
+    if (index > -1) {
+    note.splice(index, 1);
+    }
+
+    res.json(note)
+
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify(note, null, 1)
+    );
+  })
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
