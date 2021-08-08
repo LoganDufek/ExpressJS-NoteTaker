75e588479f7aaa907138212a6df51d5445ec1b48
Logan D <logandufek@gmail.com>
2021-08-08 10:23:25 -0400

    Comments added to server.js and README updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ExpressJS-NoteTaker
+## Challenge Eleven ExpressJS-NoteTaker
+
+### Description
+For this weekly challenge, the goal was to build a back end for a note taking app that would allow people to save notes, and have them persist using a server rather than local storage in their browser. This was done using the Node Express package and setting up appropriate GET, POST and DELETE routes for the information to be retrieved, presented, or removed, and updating the existing API data as well. 
+
+The GET routes involved finding and/or displaying the appropriate files/data. The POST route took whatever new information was enterd into the body, added it to the exising data in a JSON format, and then rewrote the file so that information would be accurately displayed. This is also the route where each note was assigned a unique ID based on the length of the array, to then be later used in the DELETE route. Speaking of which, the DELETE route, found the selected note using the ID, and then spliced it out of the file, with the file then being rewritten so the information would be updated on the page. 
+
+### Screenshots
+Home/Index Page
+![indexPage](./img/indexPage.png)
+
+Notes Page
+![notesPage](./img/notesPage.png)
+
+### Links
+Live Site: [Link](https://loganexpressnotetaker.herokuapp.com/) 
+Github: [Link](https://github.com/LoganDufek/ExpressJS-NoteTaker)
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Importing the necessary requirements for Node
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
@@ -5,75 +6,75 @@ const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-const  note  = require('./db/db');
+const note = require('./db/db');
 
-app.get('/notes', (req, res) => {
 
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-  
+// Get route to return the Note Taker page
+app.get('/notes', (req, res) => { // SendFile and the path for where the notes are
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+
 })
 
+// Get route for the Notes API
 app.get('/api/notes', (req, res) => {
 
-  return res.json(note);
-  
+    return res.json(note);
 })
 
-app.post('/api/notes', (req, res) => { 
-  req.body.id = note.length.toString();
+// Post route which will update the inputed information in the Notes API
+app.post('/api/notes', (req, res) => { // This garuntees each note will have a unique ID assigned to it so it can later be easily deleted
+    req.body.id = note.length.toString();
 
-  const newNote = req.body
+    // Assign the req.body info to a new variable
+    const newNote = req.body
 
+    // Log that variable for good measure and then push iitt into the exising note variable
+    console.log(newNote)
+    note.push(newNote)
 
- console.log(newNote)
- note.push(newNote)
+    // Respond with that new note variable in a JSON format
+    res.json(note);
 
+    // Then, rewrite the existing file the program will grab with that updated note variable
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(note, null, 1));
+})
 
-  res.json(note);
-
-  fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify(note, null, 1)
-    );
-  })
-
-  const findNoteByID = (id) => { 
+// Function that will find and return a matching ID for a note. It will be called later
+const findNoteByID = (id) => {
 
     const matchingID = note.find((notes) => {
-      return notes.id === id
+        return notes.id === id
     })
 
     return matchingID
-  }
+}
 
-
-  app.delete('/api/notes/:id', (req, res) => {
-    
-   
+// Delete route that will remove the note when it is clicked on
+app.delete('/api/notes/:id', (req, res) => { // Get the ID based on what the note's ID is
     const id = req.params.id
+    // Run findNoteByID using that variable
     const removedNote = findNoteByID(id)
 
     const index = note.indexOf(removedNote);
-    if (index > -1) {
-    note.splice(index, 1);
+    if (index > -1) { // If the input matches a valid ID, it will be spliced(removed) from the notes variable
+        note.splice(index, 1);
     }
 
+    // That information is then sent in a JSON format
     res.json(note)
 
-    fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify(note, null, 1)
-    );
-  })
+    // The file is then rewritten so that the delted information will be reflected in the front end of the application
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(note, null, 1));
+})
 
-
+// Get route that will return the Index/Home page of the application if anything is put into the address bar other than /notes.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
